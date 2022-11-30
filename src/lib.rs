1fb935fc392f06eafb95195bfad5bc03c29810b4
David Tolnay <dtolnay@gmail.com>
2022-11-30 15:40:36 -0800

    Implement call_site and mixed_site macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,26 @@
+use proc_macro::{Group, Span, TokenStream, TokenTree};
 
+#[proc_macro]
+pub fn call_site(input: TokenStream) -> TokenStream {
+    respan(input, Span::call_site())
+}
+
+#[proc_macro]
+pub fn mixed_site(input: TokenStream) -> TokenStream {
+    respan(input, Span::mixed_site())
+}
+
+fn respan(input: TokenStream, span: Span) -> TokenStream {
+    input
+        .into_iter()
+        .map(|mut token| {
+            if let TokenTree::Group(group) = &mut token {
+                let delimiter = group.delimiter();
+                let stream = respan(group.stream(), span);
+                *group = Group::new(delimiter, stream);
+            }
+            token.set_span(token.span().resolved_at(span));
+            token
+        })
+        .collect()
+}
