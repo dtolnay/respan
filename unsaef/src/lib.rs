0ed42326c448e46b18ede4b79bbe898be5ccc9ed
David Tolnay <dtolnay@gmail.com>
2023-04-21 22:13:41 -0700

    Add ünsafe macro

---
+++ unsaef/src/lib.rs
@@ -0,0 +1,15 @@
+#![forbid(unsafe_code)]
+#![doc(html_root_url = "https://docs.rs/unsaef/0.1.0")]
+
+use proc_macro::{Delimiter, Group, Ident, Span, TokenStream, TokenTree};
+
+#[proc_macro]
+pub fn ünsafe(input: TokenStream) -> TokenStream {
+    TokenStream::from_iter([
+        TokenTree::Ident(Ident::new(
+            &include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/lib.rs"))[10..16],
+            Span::call_site(),
+        )),
+        TokenTree::Group(Group::new(Delimiter::Brace, input)),
+    ])
+}
